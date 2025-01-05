96d223a856969dcce9f8dab84cdd38e76fd11226
Samyak Shah <samyakshah3008@gmail.com>
2025-01-05 10:49:51 +0530

    fix: undefined uri error

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
+const dotenv = require("dotenv");
+dotenv.config({ path: "../../.env" });
+
 const connectDB = async () => {
+  console.log(process.env.mongoDBURI, "uri mongodb");
+
   try {
     const connection = await mongoose.connect(process.env.mongoDBURI);
     console.log("db connected");
