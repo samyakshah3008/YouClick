9f0ce594b19bfb881db694c92e892cdd214db110
Samyak Shah <samyakshah3008@gmail.com>
2025-01-06 17:56:13 +0530

    feat - preview meta content add

--- marketing/src/app/layout.tsx
+++ marketing/src/app/layout.tsx
@@ -9,6 +9,20 @@ export const metadata: Metadata = {
   title: "YouClick",
   description:
     "Use YouClick Chrome Extension to automate like, comment and subscribe for your favorite youtuber, seamlessly binge watch whole playlist without exiting full screen and also not missing to like and comment!",
+  openGraph: {
+    title: "YouClick | Support Your Favorite YouTuber",
+    description:
+      "A chrome extension, aims to make win-win situation for both youtuber and consumer!",
+    url: "https://you-click.vercel.app/",
+    images: [
+      {
+        url: "https://https://you-click.vercel.app/thumbnail-you-click.png",
+        width: 1200,
+        height: 630,
+        alt: "YouClick Preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
