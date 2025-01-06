359177b62a9449734733bf97ea9c5ed3d8bb50d7
Samyak Shah <samyakshah3008@gmail.com>
2025-01-06 12:30:18 +0530

    feat - faqs add

--- marketing/src/constants/landing-page.ts
+++ marketing/src/constants/landing-page.ts
@@ -1,15 +1,42 @@
 const faqItems = [
   {
-    title: "Hello",
-    content: "World",
+    title: "I am not able to login to YouClick",
+    content:
+      "Please make sure you have a beta access, if you are still not able to login - please mail me at samyakshah3008@gmail.com",
   },
   {
-    title: "Hello",
-    content: "World",
+    title:
+      "I have filled Beta access form, when can I expect it to get approved?",
+    content:
+      "Thanks for showing interest, you can expect a response in 24hours. ",
   },
   {
-    title: "Hello",
-    content: "World",
+    title: "Why I cannot find YouClick in Chrome web store?",
+    content:
+      "YouClick is currently under test mode plan with only 100 users capacity. We are working to publish it on Chrome Web Store as it requires some procedures to be completed. ",
+  },
+  {
+    title:
+      "I am a YouTuber and I loved this Idea - I want to share among my community and want access in bulk.",
+    content:
+      "Thanks for showing interest, that's what the purpose of YouClick is. Since YouClick is not officially published yet please mail me at samyakshah3008@gmail.com and leave the rest on us. We will reach you out as soon as we complete the procedures.",
+  },
+  {
+    title: "Will YouClick be Free forever?",
+    content:
+      "We are planning to come up with Business plans for those who want to buy in bulk. Pay will be very minimal for sure. But we will surely have a free plan always with limited use.",
+  },
+  {
+    title:
+      "I executed like, comment and subscribe but still it is not showing.",
+    content:
+      "It has been noticed that after using YouTube APIs, it takes some time to reflect, not always but in some cases it takes a little time to show your comment or like. So please wait for some time. ",
+  },
+  {
+    title:
+      "I waited for some time after execution but still not able to see the comment.",
+    content:
+      "Please confirm that with account which you used to signup from YouClick is same as which you are using on YouTube. If it is same and still you are not able to see - please write me at samyakshah3008@gmail.com",
   },
 ];
 
